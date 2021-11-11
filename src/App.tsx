e1a7754f9c5fad064b2fdf712a7b4ff228155a94
Matheus Alves <48848963+msalves008@users.noreply.github.com>
2021-11-11 15:13:16 -0300

    resposive container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { GlobalStyles } from "./styles/global";
+import { GlobalContainer } from "./styles/container";
 
 export function App() {
   return (
     <div className="App">
-      <h1 className="title">Hello world!</h1>
+        <GlobalContainer>
+          <h1 className="title">Hello world!</h1>
+          <h2 className="title">Subtitle</h2>
+        </GlobalContainer>
       <GlobalStyles/>
     </div>
   );
